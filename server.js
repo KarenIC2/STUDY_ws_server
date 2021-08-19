5f4c5e6bf86a688889bd976defba9482ceb7a8ca
KarenIC2 <imagecheftest2@gmail.com>
2021-08-19 21:24:22 +0800

    [what]Remove unused codes

--- server.js
+++ server.js
@@ -9,25 +9,18 @@ const wss = new WebSocketServer({ port: 3000 });
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
-    console.log("received: %s", new Date().toString());
+    // console.log(message);
 
     let clients = wss.clients;
 
     clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message); 
+        client.send(message);
       }
     });
   });
 
-  //   const sendNowTime = setInterval(() => {
-  //     ws.send(String(new Date()));
-  //   }, 1000);
-
   ws.on("close", () => {
-    // clearInterval(sendNowTime);
     console.log("Close connected");
   });
-
-  ws.send("something");
 });
